d7a8e7d20fe6fe628911f1e302da32b646c5f792
eriklindqvist <erikjo82+github@gmail.com>
2013-09-20 12:23:05 +0200

    Database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.create [{ name: 'Home' }, { name: 'Food' }, { name: 'Coffee' }, { name: 'Hardware' }]
+
+Supplier.create(
+  name: 'Standout AB',
+  address: 'Västergatan 6',
+  zip: '352 30 Växjö',
+  country: 'Sweden',
+  email: 'kontakt@standout.se',
+  category: Category.find_by_name('Home')
+)
+
+Supplier.create(
+  name: '4 Krogar Vid Sjön',
+  address: 'Vattentorget 5',
+  zip: '352 34 Växjö',
+  country: 'Sweden',
+  email: 'info@4krogar.se',
+  category: Category.find_by_name('Food')
+)
+
+Supplier.create(
+  name: 'Skåres Konditori',
+  address: 'Kungsgatan 13',
+  zip: '352 31 Växjö',
+  country: 'Sweden',
+  email: 'annika@skareskonditori.se',
+  category: Category.find_by_name('Coffee')
+)
+
+Supplier.create(
+  name: 'AB Helmer Nilsson',
+  address: 'Mörnersväg 116',
+  zip: '350 53 VÄXJÖ',
+  country: 'Sweden',
+  email: 'sales@helmer-nilsson.se',
+  category: Category.find_by_name('Hardware')
+)
