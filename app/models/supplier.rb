6c5559dfa6e6179ef4fc38c8749651e03309b51b
eriklindqvist <erikjo82+github@gmail.com>
2013-09-22 10:25:54 +0200

    Email validation

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,3 +1,5 @@
 class Supplier < ActiveRecord::Base
   belongs_to :category
+  validates :category, presence: {:message => 'must be a valid category'}
+  validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, if: "email?"
 end
