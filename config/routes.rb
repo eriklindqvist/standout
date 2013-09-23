2731a19c44d35332f287b0e8ad5d204448f2046d
eriklindqvist <erikjo82+github@gmail.com>
2013-09-23 11:12:33 +0200

    Route till startsidan

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Standout::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'suppliers#index'
+  root 'application#index'
+
+  get 'about' => 'application#about'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
